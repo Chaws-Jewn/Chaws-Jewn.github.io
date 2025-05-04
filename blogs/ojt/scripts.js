917db76247cbfd1b3ee62f396ffc3b3dfbf1a974
Charles John <150521807+Chaws-Jewn@users.noreply.github.com>
2025-05-04 21:02:53 +0800

    Update scripts.js

--- blogs/ojt/scripts.js
+++ blogs/ojt/scripts.js
@@ -6,14 +6,13 @@ function init() {
 
     entries.forEach(entry => {
         const post = document.createElement('div');
-        console.log("hi");
         post.classList.add('post');
         post.innerHTML = `
             <h2>${entry.title}</h2>
+            <h3><em>${entry.date}</em></h3>
             <p>${entry.content}</p>
-            <p><strong>Author:</strong> ${entry.author}</p>
-            <p><strong>Date:</strong> ${entry.date}</p>
         `;
+        post.classList.add("flex flex-col gap-3");
         container.appendChild(post);
     });
 }
